9804c8632b6ba168127b39e7ebdf8cdbf0d98840
MN Daily products <san212128109@gmail.com>
2025-12-06 21:50:16 +0600

    Update PromoSection.jsx

--- PromoSection.jsx
+++ PromoSection.jsx
@@ -1,14 +1,1 @@
-import React from "react";
 
-function PromoSection() {
-  return (
-    <section className="promo">
-      <h2>Special Offers</h2>
-      <div className="promo-banner">
-        <p>Upto 30% off on selected items!</p>
-      </div>
-    </section>
-  );
-}
-
-export default PromoSection;
