dcb43da7eb82ce208473d8b7d9f23bd94466ad25
MN Daily products <san212128109@gmail.com>
2025-12-06 21:51:15 +0600

    Update Categories.jsx

--- Categories.jsx
+++ Categories.jsx
@@ -1,18 +1,1 @@
-import React from "react";
 
-const categories = ["Groceries", "Vegetables", "Fruits", "Snacks", "Beverages"];
-
-function Categories() {
-  return (
-    <section className="categories">
-      <h2>Categories</h2>
-      <div className="category-buttons">
-        {categories.map((cat, i) => (
-          <button key={i}>{cat}</button>
-        ))}
-      </div>
-    </section>
-  );
-}
-
-export default Categories;
