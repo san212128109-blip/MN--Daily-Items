d406ef140e21df4850ecbd633a04753c000f0cc3
MN Daily products <san212128109@gmail.com>
2025-12-06 21:52:20 +0600

    Update AdminDashboard.jsx

--- AdminDashboard.jsx
+++ AdminDashboard.jsx
@@ -1,12 +1,1 @@
-import React from "react";
 
-function AdminDashboard() {
-  return (
-    <div style={{ padding: "30px", textAlign: "center" }}>
-      <h1>Welcome, Admin!</h1>
-      <p>এখানে আপনি সমস্ত ইউজার, প্রডাক্ট এবং অর্ডার ম্যানেজ করতে পারবেন।</p>
-    </div>
-  );
-}
-
-export default AdminDashboard;
