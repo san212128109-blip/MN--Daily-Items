d4e0fc20dd076c1a653cb9a214fb9ec8b1f93acc
MN Daily products <san212128109@gmail.com>
2025-12-06 21:51:37 +0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,17 +1,1 @@
-import React, { useState } from "react";
-import RoleSelection from "./components/RoleSelection";
-import AdminLogin from "./components/AdminLogin";
-import AdminDashboard from "./components/AdminDashboard";
-import HomePage from "./components/HomePage";
 
-function App() {
-  const [role, setRole] = useState(null);
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  if (!role) return <RoleSelection setRole={setRole} />;
-  if (role === "admin" && !isAdmin) return <AdminLogin setIsAdmin={setIsAdmin} />;
-  if (role === "admin" && isAdmin) return <AdminDashboard />;
-  return <HomePage />;
-}
-
-export default App;
